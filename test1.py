5a7fa264da7785652f14eb51f42a7ad0dfbd12cd
Julia <julia.neverova@list.ru>
2022-11-11 17:41:45 +0700

    add jpg

--- test1.py
+++ test1.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("123.jpg")
-image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+import os
+ 
+for filename in os.listdir('/JPEGImages'):
+        if filename[filename.rfind(".") + 1:] in ['jpg']:
+            image = cv2.imread("*.jpg")
+            image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
 
 cv2.imshow('original Image', image)
 cv2.imshow('Normalized Image', image_norm)
