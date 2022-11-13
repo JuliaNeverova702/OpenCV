1872dbebea262cfa812b520f69baed30118b1c1d
Antonia <antoniat@student.21-school.ru>
2022-11-13 21:16:52 +0700

    all working

--- test1.py
+++ test1.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 
 import os
- 
-for filename in os.listdir('/JPEGImages'):
-        if filename[filename.rfind(".") + 1:] in ['jpg']:
-            image = cv2.imread("*.jpg")
-            image_norm = cv2.normalize(image, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
 
-cv2.imshow('original Image', image)
-cv2.imshow('Normalized Image', image_norm)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+path = "JPEGImages/"
+
+for filename in os.listdir(path):
+    if ".jpg" in filename:
+        image = cv2.imread(path + filename)
+        height, width, channels = image.shape
+        resultimage = np.zeros((height, width))
+
+        image_norm = cv2.normalize(image, resultimage, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+        isWritten = cv2.imwrite(f"JPEGnew/{filename}", image_norm)
